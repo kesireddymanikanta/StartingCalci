2491dd67288285156672936cb3f50b89c424b08e
manikanta <manikanta@192.168.0.6>
2019-05-04 06:36:29 -0700

    this is calculator programme of me

--- src/main/java/main/Calculator.java
+++ src/main/java/main/Calculator.java
@@ -37,31 +37,31 @@ public class Calculator {
     }
     public static int add(int x, int y)
     {
-    	int result=0;
+    	
 //        write your code here 
-    	result=x+y;
+    	int result=x+y;
     	
         return result;
     }
     public static int sub(int x, int y)
     {
-    	int result=0;
+    
 //        write your code here
-    	result=x-y;
+    	int result=x-y;
         return result;
     }
     public static int mult(int x, int y)
     {
-    	int result=0;
+   
 //      write your code here
-    	result=x*y;
+    	int result=x*y;
     	return result;
     }
     public static int div(int x, int y)
     {
-    	int result=0;
+  
 //      write your code here
-    	result=x/y;
+    int 	result=x/y;
     	return result;
     }
 
